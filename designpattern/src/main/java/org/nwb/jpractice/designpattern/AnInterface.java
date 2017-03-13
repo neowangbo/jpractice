576707f6c2883c9b07603900a10bc0725ec50613
neowangbo <neo.wangbo@gmail.com>
2017-03-13 23:06:34 +0800

    Start learning design pattern, because find a good book in library.

---
+++ designpattern/src/main/java/org/nwb/jpractice/designpattern/AnInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nwb.jpractice.designpattern;
+
+/**
+ *
+ * @author wangbo
+ */
+interface AnInterface {
+    
+    Object obj = System.getProperties();
+   
+    
+}
---
+++ designpattern/src/main/java/org/nwb/jpractice/designpattern/App.java
@@ -0,0 +1,13 @@
+package org.nwb.jpractice.designpattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
