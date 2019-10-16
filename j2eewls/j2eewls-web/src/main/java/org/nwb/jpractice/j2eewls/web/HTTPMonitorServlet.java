407dcba71cc42a8ce21f9067cf26a502194b0594
b5wang <neo.wangbo@gmail.com>
2019-10-16 21:16:09 +0800

    Recove the code
    Recove the code

--- j2eewls/j2eewls-web/src/main/java/org/nwb/jpractice/j2eewls/web/HTTPMonitorServlet.java
+++ j2eewls/j2eewls-web/src/main/java/org/nwb/jpractice/j2eewls/web/HTTPMonitorServlet.java
@@ -24,11 +24,13 @@ public class HTTPMonitorServlet extends HttpServlet {
 
     private static final MyLogger LOG = new MyLogger(HTTPMonitorServlet.class.getName());
 
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request,response);
     }
 
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request,response);
@@ -47,12 +49,20 @@ public class HTTPMonitorServlet extends HttpServlet {
             out.println("<h1>Servlet HTTPMonitorServlet at " + request.getContextPath() + "</h1>");
             out.println("<h4>" + request.getMethod() + "</h4>");
             out.println("<hr/>");
-            out.println("<h2>Request Headers:</h2>");
+            out.println("<h1>Request Headers:</h1>");
             Enumeration<String> headerNames = request.getHeaderNames();
             while (headerNames.hasMoreElements()) {
                 String headerName = headerNames.nextElement();
                 String headerValue = request.getHeader(headerName);
                 out.println("<h4>" + headerName + ": " + headerValue + "</h4>");
+            }
+            out.println("<hr/>");
+            out.println("<h1>Request Parameters:</h1>");
+            Enumeration<String> paramNames = request.getParameterNames();
+            while (paramNames.hasMoreElements()) {
+                String paramName = paramNames.nextElement();
+                String paramValue = request.getParameter(paramName);
+                out.println("<h4>" + paramName + ": " + paramValue + "</h4>");
             }
             out.println("</body>");
             out.println("</html>");
