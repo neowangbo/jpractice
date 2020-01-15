9c17ffe94e89a94245006e784b09a5ea24ddf61c
b5wang <neo.wangbo@gmail.com>
2020-01-15 21:32:05 +0800

    Update ArraysTest.java

--- javase/src/main/java/com/b5wang/jpractice/javase/util/ArraysTest.java
+++ javase/src/main/java/com/b5wang/jpractice/javase/util/ArraysTest.java
@@ -1,15 +1,35 @@
 package com.b5wang.jpractice.javase.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ArraysTest {
 
     public static void main(String[] args){
-        int[]  intArr = {1,8,9,4,6,3,2,7,5};
+        int[] intArr = {1,8,9,4,6,3,2,7,5};
         System.out.println(Arrays.toString(intArr));
 
+        /**
+         * How to sort an array?
+         * */
         Arrays.sort(intArr);
         System.out.println(Arrays.toString(intArr));
+
+
+        /**
+         * How to convert int array to integer array?
+         * */
+        Integer[] integerArr = Arrays.stream(intArr).boxed().toArray(Integer[]::new);
+        System.out.println(Arrays.toString(integerArr));
+        Arrays.sort(integerArr, );
+
+        /**
+         * Convert array to list
+         * */
+        List<Integer> l = Arrays.asList(intArr);
+
+
     }
 
 }
